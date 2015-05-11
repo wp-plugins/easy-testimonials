01a47acb1ce73fddd891c5a77e35ac96b3f36fe1
richardgabriel <richardgabriel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-11 16:05:45 +0000

    = 1.25.2 = * Cycle2 Update - fixes issues with additional Cycle2 plugins
    git-svn-id: https://plugins.svn.wordpress.org/easy-testimonials/trunk@1158063 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/js/easy-testimonials-cycle-fix.js
+++ include/js/easy-testimonials-cycle-fix.js
@@ -1,11 +1,35 @@
 jQuery(document).ready(function () {
-	var slideshowData = jQuery(".cycle-slideshow").data();
+	var slideshowData = jQuery(".cycle-slideshow").data();	
+	
+	var cycleSlides = "> div.testimonial_slide";
+	var cycleTimeout = "4000";
+	var cycleFx = "fade";	
+	var cycleAutoHeight = "container";
+	var cycleRandom = "false";
+	
+	if (typeof slideshowData != "undefined"){
+		if (typeof slideshowData.cycleSlides != 'undefined') {
+			var cycleSlides = slideshowData.cycleSlides;
+		} 
+		if (typeof slideshowData.cycleTimeout != 'undefined') {
+			var cycleTimeout = slideshowData.cycleTimeout;
+		}
+		if (typeof slideshowData.cycleFx != 'undefined') {
+			var cycleFx = slideshowData.cycleFx;
+		}
+		if (typeof slideshowData.slideshowData != 'undefined') {
+			var cycleAutoHeight = slideshowData.cycleAutoHeight;
+		}
+		if (typeof slideshowData.cycleRandom != 'undefined') {
+			var cycleRandom = slideshowData.cycleRandom;
+		}
+	}
 	
 	jQuery(".cycle-slideshow").cycle({
-		'slides': slideshowData.cycleSlides,
-		'timeout': slideshowData.cycleTimeout,
-		'fx': slideshowData.cycleFx,
-		'auto-height': slideshowData.cycleAutoHeight,
-		'random': slideshowData.cycleRandom
+		'slides': cycleSlides,
+		'timeout': cycleTimeout,
+		'fx': cycleFx,
+		'auto-height': cycleAutoHeight,
+		'random': cycleRandom
 	});
 });
